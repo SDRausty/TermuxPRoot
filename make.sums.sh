1d5facdcb7979bff3de9598e85c768e6028e6357
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-04 16:29:33 -0400

    modified:   make.sums.sh 	modified:   md5.sum 	new file:   sha1.sum 	modified:   sha256.sum 	modified:   sha512.sum

--- make.sums.sh
+++ make.sums.sh
@@ -3,11 +3,14 @@
 # by SDRausty https://sdrausty.github.io
 #####################################################################
 md5sum * > md5.sum
+sha1sum * > sha1.sum
 sha256sum * > sha256.sum
 sha512sum * > sha512.sum
 sed -i '/[0-9].sum/d' *.sum
 printf "\\nChecking md5sums...\\n"
 md5sum -c md5.sum
+printf "\\nChecking sha1sum...\\n"
+sha1sum -c sha1.sum
 printf "\\nChecking sha256sum...\\n"
 sha256sum -c sha256.sum
 printf "\\nChecking sha512sum...\\n"
