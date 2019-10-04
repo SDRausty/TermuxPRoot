383040fb85aeac0382dceb60dbd5d39a3ad0df5e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-04 15:29:47 -0400

    new file:   make.sums.sh

---
+++ make.sums.sh
@@ -0,0 +1,18 @@
+#!/bin/env sh 
+# Copyright 2019 (c) all rights reserved 
+# by BuildAPKs https://buildapks.github.io
+#####################################################################
+set -e -u
+
+md5sum * > md5.sum
+sha256sum * > sha256.sum
+sha512sum * > sha512.sum
+sed -i '/[0-9].sum/d' *.sum
+printf "\\nChecking md5sums...\\n"
+md5sum -c md5.sum
+printf "\\nChecking sha256sum...\\n"
+sha256sum -c sha256.sum
+printf "\\nChecking sha512sum...\\n"
+sha512sum -c sha512.sum
+
+# make.sums.sh EOF
