f68141b15f6b912327e555f7b93e299cef480baa
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-04 19:46:01 -0400

    modified:   make.sums.sh renamed:    md5.sum -> md5sum.sum renamed:    sha1.sum -> sha1sum.sum renamed:    sha256.sum -> sha256sum.sum renamed:    sha512.sum -> sha512sum.sum

--- make.sums.sh
+++ make.sums.sh
@@ -2,17 +2,17 @@
 # Copyright 2019 (c) all rights reserved 
 # by SDRausty https://sdrausty.github.io
 #####################################################################
-md5sum * > md5.sum
-sha1sum * > sha1.sum
-sha256sum * > sha256.sum
-sha512sum * > sha512.sum
-sed -i '/[0-9].sum/d' *.sum
-printf "\\nChecking md5sums...\\n"
-md5sum -c md5.sum
-printf "\\nChecking sha1sum...\\n"
-sha1sum -c sha1.sum
-printf "\\nChecking sha256sum...\\n"
-sha256sum -c sha256.sum
-printf "\\nChecking sha512sum...\\n"
-sha512sum -c sha512.sum
+for i in md5sum sha1sum sha256sum sha512sum 
+do
+	printf "\\nCreating $i...\\n"
+	$i * > $i.sum
+	sleep 0.1
+done
+sed -i '/[0-9]sum.sum/d' *.sum
+for i in md5sum sha1sum sha256sum sha512sum 
+do
+	printf "\\nChecking $i...\\n"
+	$i -c $i.sum
+	sleep 0.1
+done
 # make.sums.sh EOF
