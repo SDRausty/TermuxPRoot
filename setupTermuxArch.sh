d19e7f4e9bbe046d2a69adfb00e52f9cadf13831
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-10-27 12:38:07 -0400

    commit 683143153 on 20201027

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -5,7 +5,7 @@
 # command 'setupTermuxArch h[elp]' has information how to use this file
 ################################################################################
 IFS=$'\n\t'
-VERSIONID=2.0.298
+VERSIONID=2.0.299
 set -Eeuo pipefail
 shopt -s nullglob globstar
 umask 0022
